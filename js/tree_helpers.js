2b83a6d21c8bcf9f71eeeeb36b1ca9bd4665a42d
Jiani Fan <jiani.fan@gmail.com>
2019-12-10 17:06:45 -0800

    Before major refactoring

--- js/tree_helpers.js
+++ js/tree_helpers.js
@@ -4,20 +4,20 @@ export const camelToKebab = (string) => {
 
 export const distance = (point1, point2) => {
   return Math.sqrt((point1.x - point2.x) ** 2 + (point1.y - point2.y) ** 2);
-}
+};
 
 export const midpoint = (point1, point2) => {
   return {
     x: (point1.x + point2.x) / 2,
     y: (point1.y + point2.y) / 2
-  }
-}
+  };
+};
 
 export const randomInt = (min, max) => {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive 
-}
+};
 
 export const calculateEndPoint = (start, length, angle, ratio = 100) => {
   const radian = angle * Math.PI / 180,
@@ -26,12 +26,12 @@ export const calculateEndPoint = (start, length, angle, ratio = 100) => {
     x: start.x - newLen * Math.cos(radian),
     y: start.y - newLen * Math.sin(radian)
   };
-}
+};
 
 export const calculateEndPointOnPath = (path, ratio = 100) => {
   const length = path.length() * ratio / 100;
   return length.pointAt(length);
-}
+};
 
 export const randomPointInEllipse = (midpoint, length, width) => {
   let xCenter = midpoint.x;
@@ -44,8 +44,8 @@ export const randomPointInEllipse = (midpoint, length, width) => {
   let x = xCenter + xRadius * d * Math.cos(t);
   let y = yCenter + yRadius * d * Math.sin(t);
   
-  return { x, y }
-}
+  return { x, y };
+};
 
 export const branchAngles = (originalAngle, minAngleChange, maxAngleChange, num) => {
   const angles = [];
@@ -58,7 +58,7 @@ export const branchAngles = (originalAngle, minAngleChange, maxAngleChange, num)
   }
 
   return angles;
-}
+};
 
 export const branchingPoints = (start, end, angle, num) => {
   const length = distance(start, end);
@@ -67,8 +67,7 @@ export const branchingPoints = (start, end, angle, num) => {
   for (let i = 0; i < num - 1; i++) {
     points.push(calculateEndPoint(start, randomInt(0, length), angle));
   }
-  points.push(calculateEndPoint(start, length, angle
-    ))
+  points.push(calculateEndPoint(start, length, angle));
 
   return points;
-}
+};
