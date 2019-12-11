496b61818b48677890827e13b290383c8e8bfff8
Jiani Fan <jiani.fan@gmail.com>
2019-12-11 15:13:16 -0800

    Update leaf class

--- js/leaf.js
+++ js/leaf.js
@@ -1,28 +1,35 @@
 import * as helpers from './helpers';
 
 class Leaf {
-  constructor (canvas, color, width, length, startPt, angle, type = 'single', spread = 30) {
+  constructor (canvas, color, width, length, startPt, angle, num = 1, spread = 30) {
     this.canvas = canvas;
     this.color = color;
     this.width = width;
     this.length = length;
     this.startPt = startPt;
     this.angle = angle;
-    this.type = type;
+    this.num = num;
     this.spread = spread;
   }
 
-  drawLeaf(type) {
-    const leaf = this.canvas.line(0, 0, 0, 0);
-
-    switch (type) {
-      case 'double':
-        
-        break;
+  drawLeaf(angle = this.angle) {
+    const endPt = helpers.calculateLinearEndPoint(this.startPt, this.length, angle);
+    const leaf = this.canvas.line(this.startPt.x, this.startPt.y, endPt.x, endPt.y);
     
-      default: //defaults to single
-        break;
+    return leaf.addClass('leaf');
+  }
+
+  drawLeaves() {
+    if (this.num === 1) return this.drawLeaf();
+    
+    const increment = this.spread / this.num;
+    const startAngle = this.angle - this.spread;
+    const leaves = [];
+
+    for (let i = 0; i < this.num; i++) {
+      leaves.push(this.drawLeaf(startAngle + i * increment));
     }
+    return leaves;
   }
 }
 
