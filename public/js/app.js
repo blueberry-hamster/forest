4b883e2024071cb838e68b03d40111d22c6edde4
Jiani Fan <jiani.fan@gmail.com>
2020-01-27 15:44:10 -0800

    try endless.js

--- public/js/app.js
+++ public/js/app.js
@@ -19,3 +19,15 @@ document.addEventListener('DOMContentLoaded', (e) => {
   addTreesToForest();
 });
 
+$('.trees').endless({
+
+  direction: 'horizontal', //Direction : up (infinite top scrolling), down (infinite bottom scrolling), vertical (infinite top and bottom scrolling)
+
+  scrollbar: 'disable' //Enable or disable the scrollbar
+
+});
+
+$('#forest-container').mousewheel(function (e) {
+  $(this).scrollLeft($(this).scrollLeft() - e.deltaY * 20);
+  e.preventDefault();
+});
